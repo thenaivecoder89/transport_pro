75e41076dabb51af9df9610d695c96c5c1c16646
thenaivecoder89 <anuragsarangi13@gmail.com>
2025-10-06 16:18:03 +0400

    Updated with SQL and Python scripts to fetch Organization Master data

---
+++ application/database/Python_scripts/organization_db.py
@@ -0,0 +1,22 @@
+import json
+
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from sqlalchemy import create_engine, text
+
+# Initialize environment
+load_dotenv()
+DB_URL = os.getenv('RAILWAY_DB_URL')
+engine = create_engine(DB_URL, connect_args={'options':'-c search_path="transport_pro"'})
+
+# Select and view data
+def get_organization_master_data():
+    try:
+        organization_master_query = text('select * from organization_master')
+        with engine.connect() as conn:
+            df_organization_master = pd.read_sql(sql=organization_master_query, con=conn, index_col='org_id_pk')
+        return df_organization_master, df_organization_master.to_json()
+    except Exception as e:
+        error_message = {'Error Encountered': {e}}
+        return error_message
