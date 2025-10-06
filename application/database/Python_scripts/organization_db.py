b01200839736e227e43e436150d5d8df474c4830
thenaivecoder89 <anuragsarangi13@gmail.com>
2025-10-06 17:07:02 +0400

    Updated organization_db file to handle timestamp format

--- application/database/Python_scripts/organization_db.py
+++ application/database/Python_scripts/organization_db.py
@@ -16,6 +16,7 @@ def get_organization_master_data():
         organization_master_query = text('select * from organization_master')
         with engine.connect() as conn:
             df_organization_master = pd.read_sql(sql=organization_master_query, con=conn, index_col='org_id_pk')
+            df_organization_master['created_at'] = pd.to_datetime(df_organization_master['created_at'], unit='ms')
         return df_organization_master, df_organization_master.to_json(orient='records')
     except Exception as e:
         error_message = {'Error Encountered': {e}}
