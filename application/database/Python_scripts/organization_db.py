1e5799fb21ee90365f174bfdc14a359f98a91284
thenaivecoder89 <anuragsarangi13@gmail.com>
2025-10-09 13:45:12 +0400

    Updated database and SQL scripts - 091025

--- application/database/Python_scripts/organization_db.py
+++ application/database/Python_scripts/organization_db.py
@@ -13,7 +13,7 @@ def get_organization_master_data():
     try:
         organization_master_query = text('select * from organization_master')
         with engine.begin() as conn:
-            df_organization_master = pd.read_sql(sql=organization_master_query, con=conn, index_col='org_id_pk')
+            df_organization_master = pd.read_sql(sql=organization_master_query, con=conn)
             df_organization_master['created_at'] = df_organization_master['created_at'].astype(str)
         return df_organization_master, df_organization_master.to_json(orient='records')
     except Exception as e:
