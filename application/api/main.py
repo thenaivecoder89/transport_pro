05a7003995fd3a4ff0a51c9c9322e4ca36c2d060
thenaivecoder89 <anuragsarangi13@gmail.com>
2025-11-10 10:52:11 +0400

    Updated api package with function to get users and roles allocation.

--- application/api/main.py
+++ application/api/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import json
 from application.database.Python_scripts.organization_db import get_organization_master_data as org_master
 from application.database.Python_scripts.rbac_db import login_and_access as log_acc
+from application.database.Python_scripts.rbac_db import get_users_and_allocated_roles as usr_roles
 from application.api.api_logger import DBAccessLogMiddleware
 import os
 
@@ -20,11 +21,13 @@ app.add_middleware(
 )
 app.add_middleware(DBAccessLogMiddleware) # IPLogger to log all IP addresses that access the application.
 
+# API test
 @app.get('/', status_code=200)
 def test_railway():
     output = {'message': 'Hello World!'}
     return output
 
+# API to get organization master data
 @app.get('/get_organization_master_data', status_code=200)
 def get_organization_master_data():
     try:
@@ -33,10 +36,20 @@ def get_organization_master_data():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# API to manage RBAC
 @app.post('/login_access', status_code=200)
 def login_access(payload: dict):
     try:
         output = log_acc(payload)
         return output
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# API to get user details
+@app.get('/get_all_user_details', status_code=200)
+def get_all_user_details():
+    try:
+        all_user_detail_json = usr_roles()
+        return json.loads(all_user_detail_json)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
